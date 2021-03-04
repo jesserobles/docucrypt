1e230487b383894ac6424127e7ac8aefd5d6e01a
605682 <robles-alcaraz_jesse@bah.com>
2021-03-04 11:53:16 -0500

    Removed and regenerated api key. Now the key is read from a .env file.

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -6,8 +6,8 @@ import moment from 'moment'
 import VueGapi from 'vue-gapi'
 
 Vue.use(VueGapi, {
-  apiKey: 'AIzaSyCO2EY_VcadMkHoX4VZ0V1J3KvOPTAR8nM',
-  clientId: '1095038343404-k7kn28lhvfc174j44s0mui4aioq74ni8.apps.googleusercontent.com',
+  apiKey: process.env.VUE_APP_API_KEY,
+  clientId: process.env.VUE_APP_CLIENT_ID,
   discoveryDocs: ["https://docs.googleapis.com/$discovery/rest?version=v1", 
                   "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
   scope: "https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/drive.metadata.readonly"
